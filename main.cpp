6eb32840c45f8a64586946a25235c75e1c408579
Maris Shardyko <marrttaoburberry@gmail.com>
2024-12-14 16:49:53 +0200

    completed task 3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	return 0;
+int sum_of_range(int a, int b, int sum = 0) {
+	if (b < a) {
+		return sum;
+	}
+	return sum_of_range(a, b - 1, sum + b);
 }
+
+int main() {
+	cout << sum_of_range(1, 10);
+	return 0;
+}	
