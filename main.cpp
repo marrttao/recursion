555847ce0deacb1f731e40753e95fc9ce64d4710
Maris Shardyko <marrttaoburberry@gmail.com>
2024-12-14 16:15:01 +0200

    completed task 2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
 
+//recursion func
+
+void stars_in_row(int n) {
+	if (n == 0) {
+		return;
+	}
+	cout << "*";
+	stars_in_row(n - 1);
+}
+
 int main() {
+	int* n = new int;
+	cin >> *n;
+	stars_in_row(*n);
+
+	delete n;
 	return 0;
 }
