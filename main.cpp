82bfdd371f24f35da12e3d9444a2a6af0febd1a6
Maris Shardyko <marrttaoburberry@gmail.com>
2024-12-14 16:08:34 +0200

    completed task 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	return 0;
+int rec_pow(int n, int m)
+{
+    if (m == 0)
+        return 1;
+    return n * rec_pow(n, m - 1);
+}
+
+int main()
+{
+    int res = rec_pow(2, 3);
+    cout << res << endl;
+    return 0;
 }
