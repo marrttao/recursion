ccbe5e382bbe7db4304f67a36c97c35b46479d30
Maris Shardyko <marrttaoburberry@gmail.com>
2024-12-23 22:18:44 +0200

    Можно менять тип данных массива

--- main.cpp
+++ main.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 #include <random>
+#include <type_traits>
+#include <limits>
 
 using namespace std;
 
 template<typename T>
 void check_type(mt19937& gen, T& dis) {
-    if (typeid(T) == typeid(int)) {
-        dis = uniform_int_distribution<int>(1, 100);
+    if constexpr (is_same<T, uniform_int_distribution<int>>::value) {
+        dis.param(typename uniform_int_distribution<int>::param_type(1, 100));
     }
-    else if (typeid(T) == typeid(float)) {
-        dis = uniform_real_distribution<float>(1.0, 100.0);
+    else if constexpr (is_same<T, uniform_real_distribution<float>>::value) {
+        dis.param(typename uniform_real_distribution<float>::param_type(1.0, 100.0));
     }
-    else if (typeid(T) == typeid(double)) {
-        dis = uniform_real_distribution<double>(1.0, 100.0);
-    }
-    else if (typeid(T) == typeid(char)) {
-        dis = uniform_int_distribution<char>('a', 'z');
+    else if constexpr (is_same<T, uniform_real_distribution<double>>::value) {
+        dis.param(typename uniform_real_distribution<double>::param_type(1.0, 100.0));
     }
 }
 
@@ -23,11 +22,19 @@ template<typename T>
 T* fill_arr(int size) {
     random_device rd;
     mt19937 gen(rd());
-    T dis = check_type(gen, dis);
+    typename conditional<is_same<T, int>::value, uniform_int_distribution<int>,
+        typename conditional<is_same<T, float>::value, uniform_real_distribution<float>,
+        typename conditional<is_same<T, double>::value, uniform_real_distribution<double>,
+        uniform_int_distribution<int>>::type>::type>::type dis;
     check_type(gen, dis);
     T* arr = new T[size];
     for (int i = 0; i < size; i++) {
-        arr[i] = dis(gen);
+        if constexpr (is_same<T, char>::value) {
+            arr[i] = static_cast<char>(dis(gen) % 26 + 'a');
+        }
+        else {
+            arr[i] = dis(gen);
+        }
     }
     return arr;
 }
@@ -36,13 +43,21 @@ template<typename T>
 T** fill_two_dimensional_arr(int size) {
     random_device rd;
     mt19937 gen(rd());
-    T dis;
+    typename conditional<is_same<T, int>::value, uniform_int_distribution<int>,
+        typename conditional<is_same<T, float>::value, uniform_real_distribution<float>,
+        typename conditional<is_same<T, double>::value, uniform_real_distribution<double>,
+        uniform_int_distribution<int>>::type>::type>::type dis;
     check_type(gen, dis);
     T** two_dimensional_arr = new T * [size];
     for (int i = 0; i < size; i++) {
         two_dimensional_arr[i] = new T[size];
         for (int j = 0; j < size; j++) {
-            two_dimensional_arr[i][j] = dis(gen);
+            if constexpr (is_same<T, char>::value) {
+                two_dimensional_arr[i][j] = static_cast<char>(dis(gen) % 26 + 'a');
+            }
+            else {
+                two_dimensional_arr[i][j] = dis(gen);
+            }
         }
     }
     return two_dimensional_arr;
@@ -52,7 +67,10 @@ template<typename T>
 T*** fill_three_dimensional_arr(int size) {
     random_device rd;
     mt19937 gen(rd());
-    T dis;
+    typename conditional<is_same<T, int>::value, uniform_int_distribution<int>,
+        typename conditional<is_same<T, float>::value, uniform_real_distribution<float>,
+        typename conditional<is_same<T, double>::value, uniform_real_distribution<double>,
+        uniform_int_distribution<int>>::type>::type>::type dis;
     check_type(gen, dis);
     T*** three_dimensional_arr = new T * *[size];
     for (int i = 0; i < size; i++) {
@@ -60,7 +78,12 @@ T*** fill_three_dimensional_arr(int size) {
         for (int j = 0; j < size; j++) {
             three_dimensional_arr[i][j] = new T[size];
             for (int k = 0; k < size; k++) {
-                three_dimensional_arr[i][j][k] = dis(gen);
+                if constexpr (is_same<T, char>::value) {
+                    three_dimensional_arr[i][j][k] = static_cast<char>(dis(gen) % 26 + 'a');
+                }
+                else {
+                    three_dimensional_arr[i][j][k] = dis(gen);
+                }
             }
         }
     }
@@ -87,35 +110,32 @@ void delete_three_dimensional_arr(T*** arr, int layers, int size) {
 }
 
 template<typename T>
-T max_in_arr(T* arr, int size, int index = 0) {
-    static T max = arr[0];
+T max_in_arr(T* arr, int size, int index = 0, T max = std::numeric_limits<T>::min()) {
     if (arr[index] > max) {
         max = arr[index];
     }
     if (index == size - 1) {
         return max;
     }
-    return max_in_arr(arr, size, index + 1);
+    return max_in_arr(arr, size, index + 1, max);
 }
 
 template<typename T>
-T max_in_two_dimensional_arr(T** two_dimensional_arr, int size, int index_rows = 0, int index_cols = 0) {
-    static T max = two_dimensional_arr[0][0];
-    if (two_dimensional_arr[index_cols][index_rows] > max) {
-        max = two_dimensional_arr[index_cols][index_rows];
+T max_in_two_dimensional_arr(T** two_dimensional_arr, int size, int index_rows = 0, int index_cols = 0, T max = std::numeric_limits<T>::min()) {
+    if (two_dimensional_arr[index_rows][index_cols] > max) {
+        max = two_dimensional_arr[index_rows][index_cols];
     }
     if (index_cols == size - 1 && index_rows == size - 1) {
         return max;
     }
     if (index_rows == size - 1) {
-        return max_in_two_dimensional_arr(two_dimensional_arr, size, 0, index_cols + 1);
+        return max_in_two_dimensional_arr(two_dimensional_arr, size, 0, index_cols + 1, max);
     }
-    return max_in_two_dimensional_arr(two_dimensional_arr, size, index_rows + 1, index_cols);
+    return max_in_two_dimensional_arr(two_dimensional_arr, size, index_rows + 1, index_cols, max);
 }
 
 template<typename T>
-T max_in_three_dimensional_arr(T*** three_dimensional_arr, int size_one, int size_two, int size_three, int index_one = 0, int index_two = 0, int index_three = 0) {
-    static T max = three_dimensional_arr[0][0][0];
+T max_in_three_dimensional_arr(T*** three_dimensional_arr, int size_one, int size_two, int size_three, int index_one = 0, int index_two = 0, int index_three = 0, T max = std::numeric_limits<T>::min()) {
     if (three_dimensional_arr[index_one][index_two][index_three] > max) {
         max = three_dimensional_arr[index_one][index_two][index_three];
     }
@@ -123,13 +143,13 @@ T max_in_three_dimensional_arr(T*** three_dimensional_arr, int size_one, int siz
         return max;
     }
     if (index_three < size_three - 1) {
-        return max_in_three_dimensional_arr(three_dimensional_arr, size_one, size_two, size_three, index_one, index_two, index_three + 1);
+        return max_in_three_dimensional_arr(three_dimensional_arr, size_one, size_two, size_three, index_one, index_two, index_three + 1, max);
     }
     if (index_two < size_two - 1) {
-        return max_in_three_dimensional_arr(three_dimensional_arr, size_one, size_two, size_three, index_one, index_two + 1, 0);
+        return max_in_three_dimensional_arr(three_dimensional_arr, size_one, size_two, size_three, index_one, index_two + 1, 0, max);
     }
     if (index_one < size_one - 1) {
-        return max_in_three_dimensional_arr(three_dimensional_arr, size_one, size_two, size_three, index_one + 1, 0, 0);
+        return max_in_three_dimensional_arr(three_dimensional_arr, size_one, size_two, size_three, index_one + 1, 0, 0, max);
     }
     return max;
 }
@@ -149,11 +169,11 @@ int main() {
     int size_arr;
     cout << "Size of arr: ";
     cin >> size_arr;
-    int* arr = fill_arr<int>(size_arr);
+    float* arr = fill_arr<float>(size_arr);
     int size_two_dim_arr;
     cout << "Size of two-dimensional arr: ";
     cin >> size_two_dim_arr;
-    int** two_dimensional_arr = fill_two_dimensional_arr<int>(size_two_dim_arr);
+    char** two_dimensional_arr = fill_two_dimensional_arr<char>(size_two_dim_arr);
     int size_three_dim_arr;
     cout << "Size of three-dimensional arr: ";
     cin >> size_three_dim_arr;
