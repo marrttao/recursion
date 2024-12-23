413155948f5368833ddb64f1e821bd4583197187
Maris Shardyko <marrttaoburberry@gmail.com>
2024-12-23 22:50:54 +0200

    completed task 2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
 
+int biggest_common_divider(int a, int b, int divider = 1) {
+	if (a % divider == 0 && b % divider == 0) {
+		if (divider == a || divider == b) {
+			return divider;
+		}
+		return biggest_common_divider(a, b, divider + 1);
+	}
+	return biggest_common_divider(a, b, divider + 1);
+
+}
+
 int main() {
+	int a, b;
+	cout << "Enter two numbers: ";
+	cin >> a >> b;
+	cout << "Divider: " << biggest_common_divider(a, b) << endl;
 	return 0;
 }
